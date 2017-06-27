ee4066afc00d2e0712ae10a0b4196ff630fa84df
Rafael p <barboza.rafael.p@gmail.com>
2017-06-26 23:09:23 -0500

    Agregando recycler a proveedores y estableciendo conexion con webservice

--- app/src/main/java/com/app/master/controlinventario/Vista/AdaptadorRecicler/AdaptadorRecyclerProveedor.java
+++ app/src/main/java/com/app/master/controlinventario/Vista/AdaptadorRecicler/AdaptadorRecyclerProveedor.java
@@ -3,6 +3,7 @@ package com.app.master.controlinventario.Vista.AdaptadorRecicler;
 import android.content.Context;
 import android.support.v7.view.ActionMode;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -54,7 +55,9 @@ public class AdaptadorRecyclerProveedor extends RecyclerView.Adapter<AdaptadorRe
         return proveedores.size();
     }
 
-    public static class holder extends RecyclerView.ViewHolder {
+    public static class holder extends RecyclerView.ViewHolder implements View.OnCreateContextMenuListener,
+    View.OnClickListener,
+    MenuItem.OnMenuItemClickListener{
         private TextView nit,nombre,direccion,telefono;
 
         public holder(View itemView) {
@@ -63,6 +66,35 @@ public class AdaptadorRecyclerProveedor extends RecyclerView.Adapter<AdaptadorRe
             nombre=(TextView)itemView.findViewById(R.id.nombreProvedorCarta);
             direccion=(TextView)itemView.findViewById(R.id.direccionProvedorCarta);
             telefono=(TextView)itemView.findViewById(R.id.telefonoProvedorCarta);
+            itemView.setOnClickListener(this);
+            itemView.setOnCreateContextMenuListener(this);
+        }
+
+        @Override
+        public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+            menu.setHeaderTitle("Select The Action");
+            menu.add(0, R.id.menuEliminar, 0, "Eliminar");//groupId, itemId, order, title
+            menu.add(0, R.id.menuEditar, 0, "Editar");
+        }
+
+        @Override
+        public boolean onMenuItemClick(MenuItem item) {
+            switch (item.getItemId()){
+                case R.id.menuEliminar:
+                    Log.d("mensaje","Eliminado");
+                    return true;
+                case R.id.menuEditar:
+                    Log.d("mensaje","Editado");
+                    return true;
+                default:
+                    Log.d("mensaje","no sirve");
+            }
+            return false;
+        }
+
+        @Override
+        public void onClick(View v) {
+
         }
     }
 }
