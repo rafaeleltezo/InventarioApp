fbee19773d7be7eb447edca072bd58c02d87a795
Rafael p <barboza.rafael.p@gmail.com>
2017-06-26 22:50:04 -0500

    Agregando recycler a proveedores y estableciendo conexion con webservice

--- app/src/main/java/com/app/master/controlinventario/Vista/AdaptadorRecicler/AdaptadorRecyclerProducto.java
+++ app/src/main/java/com/app/master/controlinventario/Vista/AdaptadorRecicler/AdaptadorRecyclerProducto.java
@@ -2,8 +2,12 @@ package com.app.master.controlinventario.Vista.AdaptadorRecicler;
 
 import android.content.Context;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.view.ActionMode;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -43,12 +47,6 @@ public class AdaptadorRecyclerProducto extends RecyclerView.Adapter<AdaptadorRec
         holder.nombre.setText(producto.getNombre());
         holder.precio.setText(String.valueOf(producto.getCostoCompra()));
         Picasso.with(context).load(producto.getImagen()).into(holder.foto);
-        holder.eliminar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(context, "Eliminado", Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 
     @Override
@@ -59,14 +57,15 @@ public class AdaptadorRecyclerProducto extends RecyclerView.Adapter<AdaptadorRec
     public static class viewHolder extends RecyclerView.ViewHolder{
         private TextView nombre,codigo,precio;
         private ImageView foto;
-        private FloatingActionButton eliminar;
+
         public viewHolder(View itemView) {
             super(itemView);
             codigo=(TextView)                     itemView.findViewById(R.id.codigoProductoCarta);
             nombre=(TextView)                     itemView.findViewById(R.id.NombreCartaProducto);
             precio=(TextView)                     itemView.findViewById(R.id.PrecioCartaProducto);
             foto=(ImageView)                      itemView.findViewById(R.id.imagenCartaProducto);
-            eliminar=(FloatingActionButton)       itemView.findViewById(R.id.botonCartaProducto);
+
         }
+
     }
 }
