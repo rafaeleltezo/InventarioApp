c95935ae32cec9e33adf7c08105b37fbb03df41f
Rafael p <barboza.rafael.p@gmail.com>
2017-06-26 20:32:43 -0500

    Agregando recycler a proveedores y estableciendo conexion con webservice

--- app/src/main/java/com/app/master/controlinventario/Vista/AdaptadorRecicler/AdaptadorRecyclerProveedor.java
+++ app/src/main/java/com/app/master/controlinventario/Vista/AdaptadorRecicler/AdaptadorRecyclerProveedor.java
@@ -1,13 +1,18 @@
 package com.app.master.controlinventario.Vista.AdaptadorRecicler;
 
 import android.content.Context;
+import android.support.v7.view.ActionMode;
 import android.support.v7.widget.RecyclerView;
 import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.app.master.controlinventario.Modelo.Proveedor;
 import com.app.master.controlinventario.R;
@@ -49,8 +54,7 @@ public class AdaptadorRecyclerProveedor extends RecyclerView.Adapter<AdaptadorRe
         return proveedores.size();
     }
 
-    public static class holder extends RecyclerView.ViewHolder implements View.OnCreateContextMenuListener,
-            View.OnClickListener,AdapterView.OnItemClickListener{
+    public static class holder extends RecyclerView.ViewHolder {
         private TextView nit,nombre,direccion,telefono;
 
         public holder(View itemView) {
@@ -59,25 +63,6 @@ public class AdaptadorRecyclerProveedor extends RecyclerView.Adapter<AdaptadorRe
             nombre=(TextView)itemView.findViewById(R.id.nombreProvedorCarta);
             direccion=(TextView)itemView.findViewById(R.id.direccionProvedorCarta);
             telefono=(TextView)itemView.findViewById(R.id.telefonoProvedorCarta);
-            itemView.setOnCreateContextMenuListener(this);
-        }
-
-        @Override
-        public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-            menu.setHeaderTitle("Select The Action");
-            menu.add(0, v.getId(), 0, "Eliminar");//groupId, itemId, order, title
-            menu.add(0, v.getId(), 0, "Editar");
-
-        }
-
-        @Override
-        public void onClick(View v) {
-
-        }
-
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
         }
     }
 }
